5ecda607b5aa313c2945d732516446d3fd1132b3
KauanR <kau.matosr@gmail.com>
2022-06-30 15:48:14 -0300

    feat: header implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
-import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
-import { MatCardModule } from '@angular/material/card'
+// Components
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/header/header.component'
+// Libs
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
 
-        MatCardModule
+        MatToolbarModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
